cd7b4f1a99d128fc03f4083b544f722195ef83b5
Chuan-Chen <Chuan.Chen402@student.qcc.cuny.edu>
2023-08-08 07:49:39 -0400

    ___

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,18 @@
 import styled from "styled-components"
-
+import {ReactComponent as Logo} from '../Icons/homelogo.svg'
 
 const Page = styled.div`
 height: 100vh;
 width: 100%;
+display: grid;
 `
+
+
 export default function home(){
 
     return (
         <Page>
-            
+            <Logo style = {{justifySelf: 'center', alignSelf: 'center'}}></Logo>
         </Page>
     )
 }
